43febe6da9dbde8f38abaa26abfab23721bc6d33
Maurizio <maurizio@cannedstuff.net>
2015-07-09 18:08:14 +0200

    added scripts

---
+++ js/main.js
@@ -0,0 +1,33 @@
+var Deepnic = (function($,global,document){
+  // VARIABLES
+  var d = document,
+      w = global.innerWidth,
+      h = global.innerHeight;
+
+  function _initPlayer(){
+    $("#jquery_jplayer_1").jPlayer({
+      ready: function () {
+        $(this).jPlayer("setMedia", {
+          mp3: "audio/beside-me.mp3"
+        }).jPlayer("play");;
+      },
+      cssSelectorAncestor: "#jp_container_1",
+      swfPath: "/js/vendor",
+      supplied: "mp3"
+    });
+  }
+
+  function _init(){
+    _initPlayer();
+  }
+
+  return {
+    init: _init
+  }
+
+
+})(jQuery,window,window.document);
+
+window.onload = function(){
+  Deepnic.init();
+}
